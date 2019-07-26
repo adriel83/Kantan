4db92a58497599123cb37ef4cbcd7af965871499
Adriel de Souza <adriel.2000@hotmail.com>
2019-07-26 14:52:40 -0300

    COMMIT INICIAL

---
+++ Kantan/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Kantan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.novoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salvarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.recortarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copiarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formatarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.QuebraAutomáticaDeLinhaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.caixaTexto = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelContador = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivoToolStripMenuItem,
+            this.editarToolStripMenuItem,
+            this.formatarToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // arquivoToolStripMenuItem
+            // 
+            this.arquivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.novoToolStripMenuItem,
+            this.abrirToolStripMenuItem,
+            this.salvarToolStripMenuItem});
+            this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+            this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.arquivoToolStripMenuItem.Text = "Arquivo";
+            // 
+            // novoToolStripMenuItem
+            // 
+            this.novoToolStripMenuItem.Name = "novoToolStripMenuItem";
+            this.novoToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
+            this.novoToolStripMenuItem.Text = "Novo";
+            this.novoToolStripMenuItem.Click += new System.EventHandler(this.NovoToolStripMenuItem_Click);
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            this.abrirToolStripMenuItem.Click += new System.EventHandler(this.AbrirToolStripMenuItem_Click);
+            // 
+            // salvarToolStripMenuItem
+            // 
+            this.salvarToolStripMenuItem.Name = "salvarToolStripMenuItem";
+            this.salvarToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
+            this.salvarToolStripMenuItem.Text = "Salvar";
+            this.salvarToolStripMenuItem.Click += new System.EventHandler(this.SalvarToolStripMenuItem_Click_1);
+            // 
+            // editarToolStripMenuItem
+            // 
+            this.editarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.recortarToolStripMenuItem,
+            this.copiarToolStripMenuItem,
+            this.colarToolStripMenuItem});
+            this.editarToolStripMenuItem.Name = "editarToolStripMenuItem";
+            this.editarToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.editarToolStripMenuItem.Text = "Editar";
+            // 
+            // recortarToolStripMenuItem
+            // 
+            this.recortarToolStripMenuItem.Name = "recortarToolStripMenuItem";
+            this.recortarToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.recortarToolStripMenuItem.Text = "Recortar";
+            this.recortarToolStripMenuItem.Click += new System.EventHandler(this.RecortarToolStripMenuItem_Click);
+            // 
+            // copiarToolStripMenuItem
+            // 
+            this.copiarToolStripMenuItem.Name = "copiarToolStripMenuItem";
+            this.copiarToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.copiarToolStripMenuItem.Text = "Copiar";
+            this.copiarToolStripMenuItem.Click += new System.EventHandler(this.CopiarToolStripMenuItem_Click);
+            // 
+            // colarToolStripMenuItem
+            // 
+            this.colarToolStripMenuItem.Name = "colarToolStripMenuItem";
+            this.colarToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.colarToolStripMenuItem.Text = "Colar";
+            this.colarToolStripMenuItem.Click += new System.EventHandler(this.ColarToolStripMenuItem_Click);
+            // 
+            // formatarToolStripMenuItem
+            // 
+            this.formatarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.QuebraAutomáticaDeLinhaToolStripMenuItem});
+            this.formatarToolStripMenuItem.Name = "formatarToolStripMenuItem";
+            this.formatarToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.formatarToolStripMenuItem.Text = "Formatar";
+            // 
+            // QuebraAutomáticaDeLinhaToolStripMenuItem
+            // 
+            this.QuebraAutomáticaDeLinhaToolStripMenuItem.CheckOnClick = true;
+            this.QuebraAutomáticaDeLinhaToolStripMenuItem.Name = "QuebraAutomáticaDeLinhaToolStripMenuItem";
+            this.QuebraAutomáticaDeLinhaToolStripMenuItem.Size = new System.Drawing.Size(226, 22);
+            this.QuebraAutomáticaDeLinhaToolStripMenuItem.Text = "Quebra Automática de Linha";
+            this.QuebraAutomáticaDeLinhaToolStripMenuItem.CheckedChanged += new System.EventHandler(this.QuebraAutomáticaDeLinhaToolStripMenuItem_CheckedChanged);
+            // 
+            // caixaTexto
+            // 
+            this.caixaTexto.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.caixaTexto.Location = new System.Drawing.Point(0, 27);
+            this.caixaTexto.Name = "caixaTexto";
+            this.caixaTexto.Size = new System.Drawing.Size(800, 403);
+            this.caixaTexto.TabIndex = 1;
+            this.caixaTexto.Text = "";
+            this.caixaTexto.WordWrap = false;
+            this.caixaTexto.ZoomFactor = 1.25F;
+            this.caixaTexto.SelectionChanged += new System.EventHandler(this.CaixaTexto_SelectionChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 433);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Kantan 0.1";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // labelContador
+            // 
+            this.labelContador.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelContador.AutoSize = true;
+            this.labelContador.Location = new System.Drawing.Point(673, 433);
+            this.labelContador.Name = "labelContador";
+            this.labelContador.Size = new System.Drawing.Size(58, 13);
+            this.labelContador.TabIndex = 3;
+            this.labelContador.Text = "Ln 1, Col 1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelContador);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.caixaTexto);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Kantan ー Sem Título";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem novoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salvarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem recortarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copiarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colarToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox caixaTexto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelContador;
+        private System.Windows.Forms.ToolStripMenuItem formatarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem QuebraAutomáticaDeLinhaToolStripMenuItem;
+    }
+}
+
---
+++ Kantan/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kantan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void AbrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opd = new OpenFileDialog();
+            if (opd.ShowDialog() == DialogResult.OK)
+            {
+                label1.Text = opd.FileName.ToString();
+                Form1.ActiveForm.Text = "Kantan ー " + opd.SafeFileName;
+                try
+                {
+                    var sr = new StreamReader(opd.FileName);
+                    caixaTexto.Text = sr.ReadToEnd();
+                    sr.Close();
+                }
+                catch (SecurityException ex)
+                {
+                }
+            }
+        }
+
+        private void RecortarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            caixaTexto.Cut();
+        }
+
+        private void CopiarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            caixaTexto.Copy();
+        }
+
+        private void ColarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            caixaTexto.Paste();
+        }
+
+        private void CaixaTexto_SelectionChanged(object sender, EventArgs e)
+        {
+            String rol = (1 + caixaTexto.GetLineFromCharIndex(caixaTexto.SelectionStart)).ToString();
+            String col = (1 + caixaTexto.SelectionStart - (caixaTexto.GetFirstCharIndexFromLine(1 + caixaTexto.GetLineFromCharIndex(caixaTexto.SelectionStart) - 1))).ToString();
+            labelContador.Text = "Ln "+ rol + ", Col " + col ;
+        }
+
+        private void NovoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (caixaTexto.TextLength > 0)
+            {
+                var caixaMensagem = MessageBox.Show("Deseja salvar o arquivo?", "Salvar Arquivo?", MessageBoxButtons.YesNo);
+                if (caixaMensagem == DialogResult.Yes)
+                {
+                    salvarArquivo();
+                    caixaTexto.ResetText();
+                    label1.Text = "Kantan 0.1";
+                    Form1.ActiveForm.Text = "Kantan ー Sem Título";
+                }
+                else
+                {
+                    caixaTexto.ResetText();
+                    label1.Text = "Kantan 0.1";
+                    Form1.ActiveForm.Text = "Kantan ー Sem Título";
+                }
+            }
+        }
+        private void salvarArquivo()
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                var sw = new StreamWriter(sfd.InitialDirectory + sfd.FileName);
+                sw.Write(caixaTexto.Text);
+                sw.Close();
+            }
+        }
+
+        private void SalvarToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            salvarArquivo();
+        }
+
+        private void QuebraAutomáticaDeLinhaToolStripMenuItem_CheckedChanged(object sender, EventArgs e)
+        {
+            String texto = caixaTexto.Text;
+            caixaTexto.ResetText();
+            caixaTexto.WordWrap = !caixaTexto.WordWrap;
+            caixaTexto.Text = texto;
+        }
+    }
+}
